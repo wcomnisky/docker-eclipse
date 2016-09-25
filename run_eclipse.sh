647f99dab4419ee7d84edc4a0e91e59081fb3b27
Günter Dressel <gue@murks.at>
2016-09-25 11:09:41 +0200

    adopted run_eclipse.sh to work without sudo and use my local docker image

--- run_eclipse.sh
+++ run_eclipse.sh
@@ -1,1 +1,1 @@
-sudo docker run -it -v ~/development:/home/developer/development -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix joemat/docker-eclipse-for-rcp
+docker run -it -v ~/dev/eclipse:/home/developer/workspace -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix eclipse
