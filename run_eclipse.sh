73cab65222853b08443202f49bbb47157858e5f4
William Comnisky <william@fingermark.co.nz>
2017-01-14 13:19:14 +1300

    Fixed the script to use the current image instead of one from hub.docker.com

--- run_eclipse.sh
+++ run_eclipse.sh
@@ -1,1 +1,1 @@
-sudo docker run -it -v ~/development:/home/developer/development -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix joemat/docker-eclipse-for-rcp
+docker run -it -v ~/development:/home/developer/development -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix eclipse-rcp:luna
